a53fe505e55388691072935eddc819bd36fd5d05
GervillaMaxime <jeuxmaxime1@gmail.com>
2026-03-28 00:09:46 +0100

    feat: exercices controllers et routes

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes';
 import userRoutes from './routes/user.routes';
 import categorieRoutes from './routes/categorie.routes';
+import exerciceRoutes from './routes/exercice.routes';
 
 dotenv.config();
 
@@ -16,6 +17,8 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/categories', categorieRoutes);
+app.use('/api/exercices', exerciceRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Serveur lancé sur le port ${PORT}`);
